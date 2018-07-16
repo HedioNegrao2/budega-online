24d34dea1c48d629200eb3f972acc9a2b764ed50
hedionegrao2 <hedionegrao@gmail.com>
2018-07-15 21:32:20 -0300

    adcionando o backend

---
+++ frontend/src/common/template/dependeces.js
@@ -0,0 +1,25 @@
+//import '../../../node_modules/admin-lte/plugins/jquery/jquery.min'
+//D:\Projetos\budega-online\frontend\node_modules\admin-lte\plugins\jquery\jquery.min.js
+                                     
+import 'modules/admin-lte/plugins/jquery/jquery.min'
+import 'modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min'
+import 'modules/admin-lte/dist/js/adminlte'
+//import '../../../node_modules/admin-lte/plugins/jQueryUI/jquery-ui.min'
+//import '../../../node_modules/admin-lte/plugins/fastclick/fastclick'
+//import '../../../node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min'
+
+
+//import 'modules/admin-lte/dist/js/plugins/font-awesome/css/font-awesome.min.css'
+import 'modules/admin-lte/plugins/bootstrap/css/bootstrap.min.css'
+import 'modules/admin-lte/dist/css/adminlte.css'
+
+/////import '../../../node_modules/font-awesome/css/font-awesome.min.css'
+
+////import '../../../node_modules/ionicons/dist/css/ionicons.min.css'
+//import '../../../node_modules/admin-lte/plugins/bootstrap/css/bootstrap.min.css'
+//import '../../../node_modules/admin-lte/dist/css/adminlte.min.css'
+        
+
+//import 'modules/admin-lte/dist/css/skins/_all-skins.min.css'
+//import '../../../node_modules/admin-lte/plugins/iCheck/flat/blue.css'
+import './custom.css'
---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../common/template/dependeces'
+
+import Hader from  '../common/template/Hader'
+import Menu from  '../common/template/Menu'
+
+
+export default props => (
+    <div className='wrapper'>
+    <Hader />
+    
+    </div>
+)
